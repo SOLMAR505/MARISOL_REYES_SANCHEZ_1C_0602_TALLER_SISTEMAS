4f98516953821a378fedcf00d8fd013aa3e25c02
SOL <reyesmarisol018@gmail.com>
2021-06-09 01:25:50 -0500

    ARREGLO DE METODO

--- ARCHIVOS/archivos.c
+++ ARCHIVOS/archivos.c
@@ -1,128 +1,23 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 // Las palabras que contiene el archivo
 #define TAM_NOMBRE 100
 // Debe ser de la máxima + 1. Por ejemplo, si la máxima es 4, defínela como 5
 #define MAXIMA_LONGITUD_CADENA 10000
 
+FILE *inputFile = NULL;
+FILE *outputFile = NULL;
 int existe_archivo(char *nombre_arch);
 int crear_archivo(char *nombre_arch);
 void cerrar_archivo(FILE *ptrArchivo, char *nombre_arch);
 FILE *abrir_Archivo_lectura_escritura(char *nombre_arch);
 int tiene_datos_arch(char *nombre_arch);
 FILE *abrir_Archivo_solo_Lectura(char *nombre_arch);
-void imprimir_vector(int vector[], int tam);
 void burbuja(char arreglo[][TAM_NOMBRE], int longitud);
 int total_numeros_Arch(char *nombre_arch);
 
-int main(int argc, char const *argv[]){     
-    char inputNombre[TAM_NOMBRE];
-    char outputNombre[TAM_NOMBRE];
-    int contador = 0;
-    char buferarchivo[30];
-    int existe_arch = 0;    
-    FILE *inputFile = NULL;
-    FILE *outputFile = NULL;
-
-    if (argc > 2 && argc <= 3){
-        strcpy(inputNombre, argv[1]);
-        strcpy(outputNombre, argv[2]);
-        printf("Cantidad de argumentos: %i\n", argc);
-        printf("bin = %s\n", argv[0]);
-        printf("Argumento 1: %s\n", inputNombre);
-        printf("Argumento 2: %s\n", outputNombre);
-    } else{
-        printf("Agrega dos argumentos antes de la ejecución del programa!\n");
-        printf("\t-> Primer argumento - Archivo a Leer\n");
-        printf("\t\t-> Segundo argumento - Archivo a Escribir!\n");
-    }
-//verificar si existe el archivo o no
-    existe_arch = existe_archivo(inputNombre);
-        if(existe_arch){
-            printf("--------------------------------------------------\n");
-            printf("\tEl archivo -> %s <- YA EXISTE.\n", inputNombre);
-            }
-        else{
-            printf("---------------------------------------------------------------\n");
-            printf("\tEl archivo -> %s <- NO EXISTE.\n", inputNombre);
-            printf("---------------------------------------------------------------\n");            
-            if(crear_archivo(inputNombre)){
-                printf("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n");
-                printf("\tEl archivo -> %s <- Fue creado EXITOSAMENTE.\n", inputNombre);    
-            return -1;
-            }
-            else{
-                printf("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n");
-                printf("\tEl archivo -> %s <- NO pudo crearse.\n", inputNombre);            
-                printf("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n");
-            }
-            return -1;
-        }
-
-    if(tiene_datos_arch(inputNombre)){
-        printf("°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°\n");
-        printf("\tEL ARCHIVO -> %s <-TIENE DATOS.\n", inputNombre);     
-        printf("°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°\n");
-        printf("\n------------------------------> LECTURA <----------------------------------\n");                
-        inputFile = abrir_Archivo_solo_Lectura(inputNombre);
-        if(inputFile == NULL){
-            printf("****************************************************************\n");
-            printf("\tEl archivo -> %s <- NO pudo Abrirse.\n", inputNombre);            
-            printf("****************************************************************\n");
-        }
-        else{
-            printf("****************************************************************\n");
-            printf("\tArchivo -> %s <- Abierto Correctamente.\n", inputNombre);         
-            printf("****************************************************************\n");
-            // equivalente a la función scanf()
-            fscanf(inputFile, "%s", buferarchivo);
-            while(!feof(inputFile)){
-                printf("%s\n", buferarchivo);
-                fscanf(inputFile, "%s",buferarchivo);
-                contador++;
-            } 
-            printf("\nTotal de palabras leídas: %d\n", contador);
-            char palabras[contador+1][TAM_NOMBRE];
-            cerrar_archivo(inputFile, inputNombre);
-   
-            inputFile = abrir_Archivo_solo_Lectura(inputNombre);
-            contador = 0;
-            fscanf(inputFile, "%s", buferarchivo);
-            memcpy(palabras[contador], buferarchivo, TAM_NOMBRE);
-            while(!feof(inputFile)){
-                contador++;
-                fscanf(inputFile, "%s", buferarchivo);
-                memcpy(palabras[contador], buferarchivo, TAM_NOMBRE);
-            }
-            cerrar_archivo(inputFile, inputNombre);
-
-        
-            printf("\nImprimiendo arreglo sin ordenar\n");
-            int i;
-            for (i = 0; i < contador; i++){
-                printf("%d.  %s\n", i, palabras[i]);
-            }
-            burbuja(palabras, contador);
-
-            printf("\nImprimirendo arreglo ordenado\n");
-            for (i = 0; i < contador; i++){
-                printf("%d. %s\n", i, palabras[i]);
-            }
-        }              
-            printf("\t Total de palabras leidas = %d\n\n", contador);                    
-            printf("---------------------------------------------------------------------------\n");
-            printf("\t---> LECTURA TERMINADA <---\n");                  
-    }else{
-        printf("°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°\n");
-        printf("\tEL ARCHIVO -> %s <- NO TIENE DATOS.\n", inputNombre);
-        printf("°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°\n");
-        return -1;
-    }           
-    printf("\n");       
-    return 0;
-
-}
-
+// Implementación del método de la burbuja en C, para arreglos de cadenas
 void burbuja(char arreglo[][TAM_NOMBRE], int longitud){
     // Arreglo temporal para el intercamio de las cadenas
     char temporal[TAM_NOMBRE];
@@ -132,20 +27,122 @@ void burbuja(char arreglo[][TAM_NOMBRE], int longitud){
         for (iActual = 0; iActual < longitud - 1; iActual++){
             int iSig = iActual + 1;
 
-            // Si la cadena es mayor que la siguiente (alfabeticamente) entonces intercambian
+            // Si la cadena es menor que la siguiente (alfabeticamente) entonces intercambiamos
             if (strcmp(arreglo[iActual], arreglo[iSig]) > 0){
-                // Mueve la cadena actual a a temporal
+                // Movemos la cadena actual a la temporal
                 memcpy(temporal, arreglo[iActual], TAM_NOMBRE);
-                // Mueve al actual el siguiente elemento
+                // Movemos al actual el siguiente elemento
                 memcpy(arreglo[iActual], arreglo[iSig], TAM_NOMBRE);
-                // El siguiente elemento, lo que había antes en el actual ahora está en el temporal
+               // Y en el siguiente elemento, lo que había antes en el actual pero ahora está en temporal
                 memcpy(arreglo[iSig], temporal, TAM_NOMBRE);
             }
         }
     }
-    // No se devuelve anda
+    // No hay necesidad de devolver nada, pues modificamos al arreglo de manera interna
 }
 
+
+int main(int argc, char const *argv[]){     
+    char ingresar[TAM_NOMBRE];
+    char salida[TAM_NOMBRE];
+    int contador = 0;
+    char buferarchivo[30];
+    int existe_arch = 0;    
+
+
+    if (argc > 2 && argc <= 3){
+        strcpy(ingresar, argv[1]);
+        strcpy(salida, argv[2]);
+        printf("Cantidad de argumentos: %i\n", argc);
+        printf("programa = %s\n", argv[0]);
+        printf("Argumento 1: %s\n", ingresar);
+        printf("Argumento 2: %s\n", salida);
+    } else{
+        printf("Agrega dos argumentos antes de la ejecución del programa!\n");
+    }
+//verificar si existe el archivo o no
+    existe_arch = existe_archivo(ingresar);
+        if(existe_arch){
+            printf("--------------------------------------------------\n");
+            printf("\tEl archivo -> %s <- YA EXISTE.\n", ingresar);
+            printf("--------------------------------------------------\n");
+            }
+        else{
+            printf("---------------------------------------------------------------\n");
+            printf("\tEl archivo -> %s <- NO EXISTE.\n", ingresar);
+            printf("---------------------------------------------------------------\n");            
+            if(crear_archivo(ingresar)){
+                printf("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n");
+                printf("\tEl archivo -> %s <- Fue creado EXITOSAMENTE.\n", ingresar);    
+            }
+            else{
+                printf("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n");
+                printf("\tEl archivo -> %s <- NO pudo crearse.\n", ingresar);            
+                printf("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n");
+            }
+        }
+//se verifica si tiene DATOS y PERMITE ABRIR EL ARCHIVO
+    if(tiene_datos_arch(ingresar)){
+        printf("°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°\n");
+        printf("\tEL ARCHIVO -> %s <-TIENE DATOS.\n", ingresar);     
+        printf("°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°\n");
+        printf("\n------------------------------> LECTURA <----------------------------------\n");                
+        inputFile = abrir_Archivo_solo_Lectura(ingresar);
+        if(inputFile == NULL){
+            printf("****************************************************************\n");
+            printf("\tEl archivo -> %s <- NO pudo Abrirse.\n", ingresar);            
+            printf("****************************************************************\n");
+        }
+        else{
+            printf("****************************************************************\n");
+            printf("\tArchivo -> %s <- Abierto Correctamente.\n", ingresar);         
+            printf("****************************************************************\n");
+            // equivalente a la función scanf()
+            fscanf(inputFile, "%s", buferarchivo);
+            while(!feof(inputFile)){
+                printf("%s\n", buferarchivo);
+                fscanf(inputFile, "%s",buferarchivo);
+                contador++;
+            } 
+            char palabras[contador+1][TAM_NOMBRE];
+            cerrar_archivo(inputFile, ingresar);
+   
+            inputFile = abrir_Archivo_solo_Lectura(ingresar);
+            contador = 0;
+            fscanf(inputFile, "%s", buferarchivo);
+            memcpy(palabras[contador], buferarchivo, TAM_NOMBRE);
+            while(!feof(inputFile)){
+                contador++;
+                fscanf(inputFile, "%s", buferarchivo);
+                memcpy(palabras[contador], buferarchivo, TAM_NOMBRE);
+            }
+            cerrar_archivo(inputFile, ingresar);
+
+        
+            printf("\n----ARREGLO SIN ORDENAR----\n");
+            int i;
+            for (i = 0; i < contador; i++){
+                printf("%d.  %s\n", i, palabras[i]);
+            }
+            burbuja(palabras, contador);
+
+            printf("\n-----LISTA ORDENADA ALFABETICAMENTE----\n");
+            for (i = 0; i < contador; i++){
+                printf("%d. %s\n", i, palabras[i]);
+            }
+        }                                  
+            printf("---------------------------------------------------------------------------\n");
+            printf("\t---> LECTURA TERMINADA <---\n");                  
+    }else{
+        printf("°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°\n");
+        printf("\tEL ARCHIVO -> %s <- NO TIENE DATOS.\n", ingresar);
+        printf("°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°\n");
+    }           
+    printf("\n");       
+    return 0;
+
+}
+//METODOS PARA ABRIR ARCHIVO TXT
 int total_numeros_Arch(char *nombre_arch){
     int lee_linea = 0, contador = 0;
     FILE *ptrArchivo;
